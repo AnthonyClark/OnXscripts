449202bc394f90da22a8ff181390355d31783fd7
Anthony <anthonyclark.ac@gmail.com>
2013-02-14 23:06:43 -0500

    Updates header comment in annoy.js
    Added in a brief explanation to the header comment.

--- AnnoyFriends/annoy.js
+++ AnnoyFriends/annoy.js
@@ -4,6 +4,15 @@
 //// This script is to send texts to a target contact until a specific pass phrase is recieved
 //// by text from anybody other than the target themselves.
 //// Use in good faith!
+//
+// How does it work?
+// When this script is synced to your phone it will immediately begin sending messages to the
+// target contacts number. There is an initial sms with instructions sent to the target which
+// is followed by annoying messages on a set interval. These messages stop when a text is
+// received from a number other than the target containing the pass_phrase.
+//
+// Admittedly there is little point to this script, but it has the possibility of fun :D
+//
 
 // Initializing variables 
 var friend = {
@@ -13,8 +22,9 @@ var friend = {
 var pass_phrase = 'Never gonna give you up';
 var annoy = 'Blah Blah irritating text! Blah blah! Get somebody to help you!';
 var initial_message = 'The script has begun. It will stop when somebody other than you texts me exactly: "'+pass_phrase+'", good luck.';
-var count = 1;
-var wait_mins = 5;
+var count = 1;		//
+var wait_mins = 5;	// minutes between annoying messages
+
 // Begin the game!
 
 device.messaging.sendSms({
